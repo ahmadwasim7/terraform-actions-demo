aeaf72cce9d082545589831de3ac8f303ea83e46
Wasim Ahmad <wasim@local.com>
2025-12-15 18:14:32 +0530

    Initial Commit

---
+++ import.tf
@@ -0,0 +1,19 @@
+/*provider "aws" {
+  region = "us-east-1"
+}
+
+import {
+  to = aws_iam_user.manual_user
+  id = "anonymous"      # IAM user name (not ARN, not ID)
+}
+
+import {
+  to = aws_instance.manual_ec2
+  id = "i-02753c1839dfd1981"
+}
+
+import {
+  to = aws_security_group.manual_sg
+  id = "sg-0a1495c45466477dc"
+}*/
+
