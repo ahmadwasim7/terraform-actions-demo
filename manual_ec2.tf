f1954e25b1b3a49b769d6a72e2846db45e38d447
Wasim Ahmad <wasim@local.com>
2025-12-15 18:47:21 +0530

    wrong instance type added - 3

--- manual_ec2.tf
+++ manual_ec2.tf
@@ -10,12 +10,12 @@ resource "aws_instance" "manual_ec2" {
   region                      = "ap-southeast-1"
   ami                         = "ami-093a7f5fbae13ff67"
   associate_public_ip_address = true
-  instance_type               = "t2.micronano" 
+  instance_type               = "t2.micronano"
   #key_name                             = "my_key_pair_usa"
 
   tags = {
     Name = "Managed from Terraform"
-}
+  }
 
   #vpc_security_group_ids      = [aws_security_group.manual_sg.id]
 
