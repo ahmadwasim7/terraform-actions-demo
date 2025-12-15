9183d879d5b4e23944385e02e022845bdc69080d
Wasim Ahmad <wasim@local.com>
2025-12-15 18:51:22 +0530

    wrong instance type added - 5

--- manual_ec2.tf
+++ manual_ec2.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "manual_ec2" {
+  ami           = "ami-093a7f5fbae13ff67"
   instance_type = "t2.micro"
+
+  associate_public_ip_address = "true" # ❌ should be boolean
 
   tags = {
     Name = "Managed from Terraform"
