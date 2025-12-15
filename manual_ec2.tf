6ad2cd275b26d6315239a04205755f98b72ca9d9
Wasim Ahmad <wasim@local.com>
2025-12-15 18:49:44 +0530

    wrong instance type added - 4

--- manual_ec2.tf
+++ manual_ec2.tf
@@ -1,22 +1,8 @@
-# __generated__ by Terraform
-# Please review these resources and move them into your main configuration files.
-
-# __generated__ by Terraform
-
-provider "aws" {
-  region = "us-east-1"
-}
 resource "aws_instance" "manual_ec2" {
-  region                      = "ap-southeast-1"
-  ami                         = "ami-093a7f5fbae13ff67"
-  associate_public_ip_address = true
-  instance_type               = "t2.micronano"
-  #key_name                             = "my_key_pair_usa"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Managed from Terraform"
   }
-
-  #vpc_security_group_ids      = [aws_security_group.manual_sg.id]
-
 }
+
