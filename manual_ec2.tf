71cbe060180d12e33aa333d5e9bf7852c4da5928
Wasim Ahmad <wasim@local.com>
2025-12-16 11:59:33 +0530

    added terraform deployment workflow - 2

--- manual_ec2.tf
+++ manual_ec2.tf
@@ -1,11 +1,9 @@
 resource "aws_instance" "manual_ec2" {
-  ami           = "ami-093a7f5fbae13ff67"
+  ami           = "ami-068c0051b15cdb816"
   instance_type = "t2.micro"
 
-  associate_public_ip_address = "true" # ❌ should be boolean
-
   tags = {
-    Name = "Managed from Terraform"
+    Name = "Provisioned using Terraform through CI/CD on Githhub Actions"
   }
 }
 
