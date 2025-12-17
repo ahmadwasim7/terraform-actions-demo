8e8b2f40fbc0bc74b94c8770e5005e21237f465d
ahmadwasim7077 <ahmadwasim7077@gmail.com>
2025-12-17 12:10:38 +0530

    added env in tags

--- manual_ec2.tf
+++ manual_ec2.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "manual_ec2" {
   tags = {
     Name        = "Ubuntu Server"
     Description = "Provisioned using Terraform through CI/CD on Githhub Actions"
+    Environment = "Production"
   }
 }
 
