c775d9dd08d7420236ca744b13273d303f68377f
ahmadwasim7077 <ahmadwasim7077@gmail.com>
2025-12-18 17:09:06 +0530

    test: terraform pr exploit simulation - 3

--- attack.tf
+++ attack.tf
@@ -2,11 +2,21 @@ resource "null_resource" "proof_of_secret_access" {
   provisioner "local-exec" {
     command = <<EOT
 echo "=== ATTACK SIMULATION ==="
-echo "AWS_ACCESS_KEY_ID is set? -> ${AWS_ACCESS_KEY_ID:+YES}"
-echo "AWS_SECRET_ACCESS_KEY is set? -> ${AWS_SECRET_ACCESS_KEY:+YES}"
-echo "Terraform ran attacker-controlled code"
+
+if [ -n "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is set -> YES"
+else
+  echo "AWS_ACCESS_KEY_ID is set -> NO"
+fi
+
+if [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is set -> YES"
+else
+  echo "AWS_SECRET_ACCESS_KEY is set -> NO"
+fi
+
+echo "Terraform executed attacker-controlled code"
 echo "========================="
 EOT
   }
 }
-#added comments
