1ef5259dc8f301b22a3b35a69929c187ed8aae64
Wasim Ahmad <wasim@local.com>
2025-12-21 11:13:47 +0530

    added env var in workflow

--- manual_ec2.tf
+++ manual_ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "manual_ec2" {
-  ami           = "ami-068c0051b15cdb816"
+  ami           = "ami-00ca570c1b6d79f36"
   instance_type = "t2.micro"
 
   tags = {
     Name        = "Amazon EC2 Server"
-    Description = "Provisioned using Terraform through CI/CD on Githhub Actions"
+    Description = "Provisioned using Github Actions CI/CD through Terraform"
     Environment = "Prod"
   }
 }
