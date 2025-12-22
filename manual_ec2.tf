b54b0d9c0f458e51ca145b980d9e88d7ddb97729
Wasim Ahmad <wasim@local.com>
2025-12-22 12:57:27 +0530

    added conditional github actions workflow-1

--- manual_ec2.tf
+++ manual_ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "manual_ec2" {
   ami           = "ami-00ca570c1b6d79f36"
   instance_type = "t2.micro"
 
-#  tags = {
-#    Environment = var.environment
-#  }
+  tags = {
+    Environment = var.environment
+  }
 }
